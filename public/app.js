bf65f0d10ea2fc49170686761b24c2bfcfda8f9e
Donna <donnaw@gmail.com>
2018-01-18 23:03:55 +0000

    added views and public folders and started jquery

---
+++ public/app.js
@@ -0,0 +1,14 @@
+/* global $ */
+
+$(document).ready(function() {
+  $.getJSON("/api/todos")
+  .then(addTodos);
+}); 
+
+function addTodos(todos) {
+  // add todos to page here
+  todos.forEach(function(todo){
+    var newTodo = $('<li class="task">' + todo.name + '</li>');
+    $('.list').append(newTodo);
+  })
+}
